af82d4445f54ef471b90929b1d1a56d6e774a85c
Pulkit Jalan <pulkit1990@gmail.com>
2016-03-11 08:52:33 +0000

    format

--- src/Commands/MultiServerMigrationService.php
+++ src/Commands/MultiServerMigrationService.php
@@ -5,31 +5,52 @@ namespace jdavidbakr\MultiServerEvent\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
-class MultiServerMigrationService extends Command {
+class MultiServerMigrationService extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'make:migration:multi-server-event';
 
-	protected $signature = 'make:migration:multi-server-event';
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Generate a migration for managing multi server events';
 
-	protected $description = 'Generate a migration for managing multi server events';
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
 
-	protected $files;
-
-	public function __construct()
-	{
-		$this->files = new Filesystem();
-		return parent::__construct();
-	}
-
-	public function handle()
-	{
-		$migration_file = './database/migrations/' . date('Y_m_d_His') . '_multi-server-event.php';
-		$this->files->put($migration_file, $this->compileMigrationStub());
-	}
-
-	protected function compileMigrationStub()
+    /**
+     * Generate a migration for managing multi server events.
+     */
+    public function __construct()
     {
-        $stub = $this->files->get(__DIR__ . '/../stubs/migration.stub');
+        parent::__construct();
+        $this->files = new Filesystem();
+    }
+
+    /**
+     * Execute the console command.
+     * @return mixed
+     */
+    public function handle()
+    {
+        $migration_file = './database/migrations/'.date('Y_m_d_His').'_multi-server-event.php';
+        $this->files->put($migration_file, $this->compileMigrationStub());
+    }
+
+    /**
+     * Get the migration stub.
+     * @return string
+     */
+    protected function compileMigrationStub()
+    {
+        $stub = $this->files->get(__DIR__.'/../stubs/migration.stub');
 
         return $stub;
     }
 }
-?>
--- src/Scheduling/Event.php
+++ src/Scheduling/Event.php
@@ -4,30 +4,33 @@ namespace jdavidbakr\MultiServerEvent\Scheduling;
 
 use Illuminate\Console\Scheduling\Event as NativeEvent;
 use Illuminate\Contracts\Container\Container;
-use DB;
+use Illuminate\Support\Facades\DB;
 
-class Event extends NativeEvent {
+class Event extends NativeEvent
+{
+    /**
+     * Hash we will use to identify our server and lock the process.
+     * @var string
+     */
+    protected $server_id;
 
-	/**
-	 * Hash we will use to identify our server and lock the process
-	 * @var string
-	 */
-	protected $server_id;
-	/**
-	 * Hash of the command mutex we will use to uniquely identify the command
-	 * @var string
-	 */
-	protected $key;
-	/**
-	 * The database connection for the multi_server_event table
-	 * @var string
-	 */
-	public $connection = null;
-	/**
-	 * The name of the table that contains the process locks
-	 * @var string
-	 */
-	public $lock_table = 'multi_server_event';
+    /**
+     * Hash of the command mutex we will use to uniquely identify the command.
+     * @var string
+     */
+    protected $key;
+    
+    /**
+     * The database connection for the multi_server_event table.
+     * @var string
+     */
+    public $connection = null;
+    
+    /**
+     * The name of the table that contains the process locks.
+     * @var string
+     */
+    public $lock_table = 'multi_server_event';
 
     /**
      * Create a new event instance.
@@ -37,75 +40,86 @@ class Event extends NativeEvent {
      */
     public function __construct($command)
     {
-    	parent::__construct($command);
-		$this->server_id = str_random(32);
+        parent::__construct($command);
+        $this->server_id = str_random(32);
     }
 
     /**
-     * Run the given event, then clear our lock
+     * Run the given event, then clear our lock.
      *
      * @param  \Illuminate\Contracts\Container\Container  $container
      * @return void
      */
     public function run(Container $container)
     {
-    	parent::run($container);
-		$this->clearMultiserver();
-	}
+        parent::run($container);
+        $this->clearMultiserver();
+    }
 
-	/**
-	 * Prevents this command from executing across multiple servers attempting
-	 * at the same time
-	 * @return [type] [description]
-	 */
-	public function withoutOverlappingMultiServer()
-	{
-		return $this->skip(function() {
-			return $this->skipMultiserver();
-		});
-	}
+    /**
+     * Prevents this command from executing across multiple servers attempting
+     * at the same time.
+     * @return $this
+     */
+    public function withoutOverlappingMultiServer()
+    {
+        return $this->skip(function () {
+            return $this->skipMultiserver();
+        });
+    }
 
-	/**
-	 * Attempt to lock this command 
-	 * @return bool true if we want to skip
-	 */
-	public function skipMultiserver()
-	{
-		$this->key = md5($this->expression.$this->command);
-		// Delete any old completed runs that are more than 10 seconds ago
-		DB::connection($this->connection)
-			->delete('delete from `'.$this->lock_table.'` where `mutex` = ? and complete < now() - interval 10 second',
-				[$this->key]);
-		// Attempt to acquire the lock
-		DB::connection($this->connection)
-			->insert('insert ignore into `'.$this->lock_table.'` set `mutex` = ?, `lock` = ?, `start` = now()',
-				[$this->key, $this->server_id]);
-		// If the mutex already exists in the table, the above query will fail silently.
-		// Now we will perform a select to see if we got the lock or not.
-		$lock = DB::connection($this->connection)
-			->select('select `lock` from `'.$this->lock_table.'` where mutex = ?',
-				[$this->key]);
-		if($lock[0]->lock == $this->server_id) {
-			// We got the lock
-			return false;
-		} else {
-			// Someone else has the lock
-			return true;			
-		}
-	}
+    /**
+     * Attempt to lock this command.
+     * @return bool true if we want to skip
+     */
+    public function skipMultiserver()
+    {
+        $this->key = md5($this->expression.$this->command);
 
-	/**
-	 * Delete our locks
-	 * @return void 
-	 */
-	public function clearMultiserver()
-	{
-		// Clear the lock
-		if($this->server_id) {
-			DB::connection($this->connection)
-				->update('update `'.$this->lock_table.'` set complete = now() where `mutex` = ? and `lock` = ?',
-					[$this->key, $this->server_id]);
-		}
-	}
-	
+        // Delete any old completed runs that are more than 10 seconds ago
+        DB::connection($this->connection)
+            ->delete(
+                'delete from `'.$this->lock_table.'` where `mutex` = ? and complete < now() - interval 10 second',
+                [$this->key]
+            );
+
+        // Attempt to acquire the lock
+        DB::connection($this->connection)
+            ->insert(
+                'insert ignore into `'.$this->lock_table.'` set `mutex` = ?, `lock` = ?, `start` = now()',
+                [$this->key, $this->server_id]
+            );
+
+        // If the mutex already exists in the table, the above query will fail silently.
+        // Now we will perform a select to see if we got the lock or not.
+        $lock = DB::connection($this->connection)
+            ->select(
+                'select `lock` from `'.$this->lock_table.'` where mutex = ?',
+                [$this->key]
+            );
+
+        if ($lock[0]->lock == $this->server_id) {
+            // We got the lock
+            return false;
+        }
+
+        // Someone else has the lock
+        return true;
+    }
+
+    /**
+     * Delete our locks.
+     * @return void 
+     */
+    public function clearMultiserver()
+    {
+        // Clear the lock
+        if ($this->server_id) {
+            DB::connection($this->connection)
+                ->update(
+                    'update `'.$this->lock_table.'` set complete = now() where `mutex` = ? and `lock` = ?',
+                    [$this->key, $this->server_id]
+                );
+        }
+    }
 }
